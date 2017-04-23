6e0361666969179c56e231c5879387241d5e481e
YeisonValero0o141 <valeroyeison97@gmail.com>
2017-04-23 16:06:20 -0400

    Finish test for snake.

--- src/game_test.py
+++ src/game_test.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -17,7 +18,7 @@ from snake import Snake
 
 pygame.init()
 
-# some objects needed by some objects
+# some objects needed by Snake class
 settings = Settings()
 size = (settings.screen_width, settings.screen_height)
 screen = pygame.display.set_mode(size)
@@ -41,6 +42,7 @@ class TestSnake(unittest.TestCase):
             expected_len = len_snake + x
             snake.increase_lenght()
             self.assertEqual(len(snake), expected_len, msg='the increase failed beacause {0} is not equal than {1}'.format(len(snake), expected_len))
+
 
     def test_collide_with_walls(self):
         """Test does_collide_with_walls method."""
@@ -72,12 +74,54 @@ class TestSnake(unittest.TestCase):
 
     def test_achieve_walls(self):
         """Test achive_walls method."""
+        positions = [
+            [-45, 525], [600, 5],
+            [-12, 1000], [1903, 5]
+        ]
+        for i, pos in enumerate(positions):
+            snake = Snake(screen, settings, fs, '../sounds/mordisco.wav')
+            head = settings.snake_head
+            # take x position
+            if i <= 1:
+                head_pos = getattr(head, 'rect')[1]
+            # take y positoins
+            else:
+                head_pos = getattr(head, 'rect')[0]
+
+            # just change  position of snake's head
+            if i <= 1:
+                head.rect.y = pos[0]
+            else:
+                head.rect.x = pos[0]
+
+            # call method to verify if snake achieve wall,
+            # if so, change its position
+            snake.achieve_walls()
+
+            if i <= 1:
+                self.assertEqual(head.rect.y, pos[1], msg='{0} and {1} are different positions.'.format(head.rect.y, pos[1]))
+            else:
+                self.assertEqual(head.rect.x, pos[1], msg='{0} and {1} are different positions.'.format(head.rect.x, pos[1]))
+
+
+    def test_moves(self):
+        """test move_up, move_down, move_left and move_right."""
+        change_moves = [
+            (-18, 0,), (18, 0,),
+            (0, -18,), (0, 18,)
+        ]
         snake = Snake(screen, settings, fs, '../sounds/mordisco.wav')
-        head = settings.snake_head
-        # change position of snake's head
-        head.rect.y = -45
-        snake.achieve_walls()
-        self.assertEqual(head.rect.y, 525)
+        moves_methods = [
+            (snake.move_up), (snake.move_down),
+            (snake.move_left), (snake.move_right)
+        ]
+        for ch_move, m_method in zip(change_moves, moves_methods):
+            # call function to move snake
+            m_method()
+            # take the change of movement of snake
+            move = (settings.change_position_y, settings.change_position_x)
+
+            self.assertTupleEqual(move, ch_move, msg='{0} and {1} are not equal.'.format(move, ch_move))
 
 
 
